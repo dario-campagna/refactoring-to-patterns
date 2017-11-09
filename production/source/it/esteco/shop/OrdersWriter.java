3edbfba352dc561771a4cc7dd44bf3e714ca746d
Dario Campagna <campagna@esteco.com>
2017-11-09 17:19:41 +0530

    Apply Compose Method and Move Accumulation to Collecting Parameter

--- production/source/it/esteco/shop/OrdersWriter.java
+++ production/source/it/esteco/shop/OrdersWriter.java
@@ -10,6 +10,11 @@ public class OrdersWriter {
 
     public String getContents() {
         StringBuffer xml = new StringBuffer();
+        writeOrderTo(xml);
+        return xml.toString();
+    }
+
+    private void writeOrderTo(StringBuffer xml) {
         xml.append("<orders>");
         for (int i = 0; i < orders.getOrderCount(); i++) {
             Order order = orders.getOrder(i);
@@ -17,34 +22,41 @@ public class OrdersWriter {
             xml.append(" id='");
             xml.append(order.getOrderId());
             xml.append("'>");
-            for (int j = 0; j < order.getProductCount(); j++) {
-                Product product = order.getProduct(j);
-                xml.append("<product");
-                xml.append(" id='");
-                xml.append(product.getID());
-                xml.append("'");
-                xml.append(" color='");
-                xml.append(getColorFor(product));
-                xml.append("'");
-                if (product.getSize() != ProductSize.NOT_APPLICABLE) {
-                    xml.append(" size='");
-                    xml.append(getSizeFor(product));
-                    xml.append("'");
-                }
-                xml.append(">");
-                xml.append("<price");
-                xml.append(" currency='");
-                xml.append(getCurrencyFor(product));
-                xml.append("'>");
-                xml.append(product.getPrice());
-                xml.append("</price>");
-                xml.append(product.getName());
-                xml.append("</product>");
-            }
+            writeProductsTo(xml, order);
             xml.append("</order>");
         }
         xml.append("</orders>");
-        return xml.toString();
+    }
+
+    private void writeProductsTo(StringBuffer xml, Order order) {
+        for (int j = 0; j < order.getProductCount(); j++) {
+            Product product = order.getProduct(j);
+            xml.append("<product");
+            xml.append(" id='");
+            xml.append(product.getID());
+            xml.append("'");
+            xml.append(" color='");
+            xml.append(getColorFor(product));
+            xml.append("'");
+            if (product.getSize() != ProductSize.NOT_APPLICABLE) {
+                xml.append(" size='");
+                xml.append(getSizeFor(product));
+                xml.append("'");
+            }
+            xml.append(">");
+            writePriceTo(xml, product);
+            xml.append(product.getName());
+            xml.append("</product>");
+        }
+    }
+
+    private void writePriceTo(StringBuffer xml, Product product) {
+        xml.append("<price");
+        xml.append(" currency='");
+        xml.append(getCurrencyFor(product));
+        xml.append("'>");
+        xml.append(product.getPrice());
+        xml.append("</price>");
     }
 
     private String getCurrencyFor(Product product) {
